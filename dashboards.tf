343d2790a073b0d9e12c171520857404d8385c9b
Steve Beattie <stevan.beattie@digital.cabinet-office.gov.uk>
2023-12-13 14:02:40 +0000

    Added new dashboard for team-idc-app

--- dashboards.tf
+++ dashboards.tf
@@ -51,3 +51,25 @@ resource "dynatrace_dashboard_sharing" "slos" {
     }
   }
 }
+
+resource "dynatrace_json_dashboard" "team-idc-app" {
+  contents = file("${path.module}/dashboards/team-idc-app.json")
+}
+
+resource "dynatrace_dashboard_sharing" "team-idc-app" {
+  dashboard_id = dynatrace_json_dashboard.team-idc-app.id
+
+  enabled = true
+
+  permissions {
+    permission {
+      level = "VIEW"
+      type  = "ALL"
+    }
+    permission {
+      id    = data.dynatrace_iam_group.all.id
+      level = "VIEW"
+      type  = "GROUP"
+    }
+  }
+}
