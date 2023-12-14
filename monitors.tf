c448df410eec96abc8edec99e39041b014ee1a4c
Joe Roberts <joe.roberts@digital.cabinet-office.gov.uk>
2023-12-14 09:21:27 +0000

    Fix commas, note to self: terraform validate in CI

--- monitors.tf
+++ monitors.tf
@@ -1,12 +1,12 @@
 module "oidc-jwks-monitor" {
   for_each = toset(var.environment == "production" ? [
-    "api.identity.account.gov.uk"
+    "api.identity.account.gov.uk",
     "api-backend-api.review-b.account.gov.uk",
     "oidc.account.gov.uk"
     ] : flatten([for env in local.aws_environments : [
-      "api.identity.${env}.account.gov.uk"
+      "api.identity.${env}.account.gov.uk",
       "api-backend-api.review-b.${env}.account.gov.uk",
-      "oidc.${env}.account.gov.uk",
+      "oidc.${env}.account.gov.uk"
   ]]))
 
   source      = "./modules/monitors/jwks"
