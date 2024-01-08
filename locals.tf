90ecc28f3d9b6cdc120a1a3d1d62c3dd99f6fa14
Pavels <pavels.zaborovskis@digital.cabinet-office.gov.uk>
2024-01-08 14:37:33 +0000

    OBS-185 add preferences configs for env settings

--- locals.tf
+++ locals.tf
@@ -2,4 +2,5 @@ locals {
   accounts = { for account in csvdecode(file("${path.module}/accounts.csv")) : account.name => account }
 
   aws_environments = var.environment == "production" ? ["production"] : ["build", "staging", "integration"]
+  is_production = contains(local.aws_environments, terraform.workspace)
 }
