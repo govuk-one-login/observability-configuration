899780eaabe800fc87a2a46cc089f1e6fda40d38
Pavels <146743710+Pave1s@users.noreply.github.com>
2024-01-16 13:08:35 +0000

    OBS-161 add service detection for YOTI

---
+++ service_detection.tf
@@ -0,0 +1,26 @@
+resource "dynatrace_service_external_web_request" "YOTI" {
+  name    = "YOTI"
+  enabled = true
+  conditions {
+    condition {
+      attribute              = "HostName"
+      compare_operation_type = "StringEndsWith"
+      text_values            = local.is_production ? ["proxy.review-o.account.gov.uk"] : ["yotistub.review-o.staging.account.gov.uk"]
+    }
+  }
+  id_contributors {
+    port_for_service_id = false
+    application_id {
+      enable_id_contributor = false
+    }
+    context_root {
+      enable_id_contributor = false
+    }
+    public_domain_name {
+      enable_id_contributor = true
+      service_id_contributor {
+        contribution_type = "OriginalValue"
+      }
+    }
+  }
+}
