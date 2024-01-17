1b087606ef0347e3753647f7b615f1fb2c87c8e5
chrisdodd93 <christopher.dodd@digital.cabinet-office.gov.uk>
2024-01-17 14:01:48 +0000

    IaC for config required for alerting

--- problem_notifications.tf
+++ problem_notifications.tf
@@ -2,7 +2,7 @@ resource "dynatrace_slack_notification" "slack_notification" {
     active  = true
     name    = "di-slack-notification"
     profile = "di-alerting-profile"
-    url     = local.is_production ? "https://hooks.slack.com/services/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" : "https://hooks.slack.com/services/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"
-    channel = local.is_production ? "#di-dynatrace-prod-alerts" : "#di-dynatrace-alerts"
+    url     = local.is_production ? jsondecode(data.aws_secretsmanager_secret_version.pr.secret_string)["SLACK_URL"] : jsondecode(data.aws_secretsmanager_secret_version.np.secret_string)["SLACK_URL"]
+    channel = local.is_production ? jsondecode(data.aws_secretsmanager_secret_version.pr.secret_string)["SLACK_CHANNEL"] : jsondecode(data.aws_secretsmanager_secret_version.np.secret_string)["SLACK_CHANNEL"]
     message = "{State} {ProblemSeverity} Problem {ProblemID}: {ImpactedEntity}"
 }
