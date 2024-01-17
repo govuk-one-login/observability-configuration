4f1c1bb9a83d22d8475377920ae36ddb083b6059
NicolaCassidy <nicola.cassidy@hotmail.co.uk>
2024-01-17 10:57:06 +0000

    Adding Service Detection for External Web Requests

--- service_detection.tf
+++ service_detection.tf
@@ -24,3 +24,241 @@ resource "dynatrace_service_external_web_request" "YOTI" {
     }
   }
 }
+resource "dynatrace_service_external_web_request" "Zendesk" {
+  name    = "Zendesk"
+  enabled = true
+  conditions {
+    condition {
+      count                  = local.is_production ? 1 : 0
+      attribute              = "URLHostName"
+      compare_operation_type = "EndsWith"
+      ignore_case            = false
+      text_values            = ["zendesk.com"]
+    }
+  }
+  id_contributors {
+    port_for_service_id = false
+    application_id {
+      enable_id_contributor = false
+      service_id_contributor {
+        contribution_type = "OriginalValue"
+      }
+    }
+    context_root {
+      enable_id_contributor = false
+      service_id_contributor {
+        contribution_type = "OriginalValue"
+      }
+    }
+    public_domain_name {
+      enable_id_contributor = true
+      service_id_contributor {
+        contribution_type   = "OriginalValue"
+        copy_from_host_name = true
+        transformations {
+          transformation {
+            transformation_type = "BEFORE"
+          }
+        }
+      }
+    }
+  }
+}
+resource "dynatrace_service_external_web_request" "Crosscore API" {
+  name    = "Crosscore API"
+  enabled = true
+  conditions {
+    condition {
+      attribute              = "URLHostName"
+      compare_operation_type = "EndsWith"
+      ignore_case            = false
+      text_values            = local.is_production ? ["api.crosscore.uk.experian.com"] : ["api.uat.crosscore.uk.experian.com"]
+    }
+  }
+  id_contributors {
+    port_for_service_id = false
+    application_id {
+      enable_id_contributor = false
+      service_id_contributor {
+        contribution_type = "OriginalValue"
+      }
+    }
+    context_root {
+      enable_id_contributor = false
+      service_id_contributor {
+        contribution_type = "OriginalValue"
+      }
+    }
+    public_domain_name {
+      enable_id_contributor = true
+      service_id_contributor {
+        contribution_type   = "OriginalValue"
+        copy_from_host_name = true
+        transformations {
+          transformation {
+            transformation_type = "BEFORE"
+          }
+        }
+      }
+    }
+  }
+}
+resource "dynatrace_service_external_web_request" "HMPO API" {
+  name    = "HMPO API"
+  enabled = true
+  conditions {
+    condition {
+      attribute              = "URLHostName"
+      compare_operation_type = "EndsWith"
+      ignore_case            = false
+      text_values            = local.is_production ? ["api.dvadigital.homeoffice.gov.uk"] : ["api-dvadigital-cte.np.homeoffice.gov.uk"]
+    }
+  }
+  id_contributors {
+    port_for_service_id = false
+    application_id {
+      enable_id_contributor = false
+      service_id_contributor {
+        contribution_type = "OriginalValue"
+      }
+    }
+    context_root {
+      enable_id_contributor = false
+      service_id_contributor {
+        contribution_type = "OriginalValue"
+      }
+    }
+    public_domain_name {
+      enable_id_contributor = true
+      service_id_contributor {
+        contribution_type   = "OriginalValue"
+        copy_from_host_name = true
+        transformations {
+          transformation {
+            transformation_type = "BEFORE"
+          }
+        }
+      }
+    }
+  }
+}
+resource "dynatrace_service_external_web_request" "DVA API" {
+  name    = "DVA API"
+  enabled = true
+  conditions {
+    condition {
+      count                  = local.is_production ? 1 : 0
+      attribute              = "URLHostName"
+      compare_operation_type = "EndsWith"
+      ignore_case            = false
+      text_values            = ["driverlicensingcheck.nidirect.gov.uk"]
+    }
+  }
+  id_contributors {
+    port_for_service_id = false
+    application_id {
+      enable_id_contributor = false
+      service_id_contributor {
+        contribution_type = "OriginalValue"
+      }
+    }
+    context_root {
+      enable_id_contributor = false
+      service_id_contributor {
+        contribution_type = "OriginalValue"
+      }
+    }
+    public_domain_name {
+      enable_id_contributor = true
+      service_id_contributor {
+        contribution_type   = "OriginalValue"
+        copy_from_host_name = true
+        transformations {
+          transformation {
+            transformation_type = "BEFORE"
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "dynatrace_service_external_web_request" "DVLA API" {
+  name    = "DVLA API"
+  enabled = true
+  conditions {
+    condition {
+      attribute              = "URLHostName"
+      compare_operation_type = "EndsWith"
+      ignore_case            = false
+      text_values            = local.is_production ? ["driver-vehicle-licensing.api.gov.uk"] : ["uat.driver-vehicle-licensing.api.gov.uk"]
+    }
+  }
+  id_contributors {
+    port_for_service_id = false
+    application_id {
+      enable_id_contributor = false
+      service_id_contributor {
+        contribution_type = "OriginalValue"
+      }
+    }
+    context_root {
+      enable_id_contributor = false
+      service_id_contributor {
+        contribution_type = "OriginalValue"
+      }
+    }
+    public_domain_name {
+      enable_id_contributor = true
+      service_id_contributor {
+        contribution_type   = "OriginalValue"
+        copy_from_host_name = true
+        transformations {
+          transformation {
+            transformation_type = "BEFORE"
+          }
+        }
+      }
+    }
+  }
+}
+resource "dynatrace_service_external_web_request" "AWS" {
+  name    = "AWS"
+  enabled = true
+  conditions {
+    condition {
+      count                  = local.is_production ? 1 : 0
+      attribute              = "URLHostName"
+      compare_operation_type = "EndsWith"
+      ignore_case            = false
+      text_values            = ["amazonaws.com"]
+    }
+  }
+  id_contributors {
+    port_for_service_id = false
+    application_id {
+      enable_id_contributor = false
+      service_id_contributor {
+        contribution_type = "OriginalValue"
+      }
+    }
+    context_root {
+      enable_id_contributor = false
+      service_id_contributor {
+        contribution_type = "OriginalValue"
+      }
+    }
+    public_domain_name {
+      enable_id_contributor = true
+      service_id_contributor {
+        contribution_type   = "OriginalValue"
+        copy_from_host_name = true
+        transformations {
+          transformation {
+            transformation_type = "BEFORE"
+          }
+        }
+      }
+    }
+  }
+}
