af8bf1c53724785085337b749cbb82086a0acf52
Pavels <pavels.zaborovskis@digital.cabinet-office.gov.uk>
2024-01-19 09:47:47 +0000

    OBS-186 fix existing code add external web requests and unified services endpoint metrics

--- data.tf
+++ data.tf
@@ -1,15 +1,15 @@
 data "aws_secretsmanager_secret" "np" {
-    arn = "arn:aws:secretsmanager:eu-west-2:216552277552:secret:DynatraceNonProductionSlackIntegration-KTAK1u"
+  arn = "arn:aws:secretsmanager:eu-west-2:216552277552:secret:DynatraceNonProductionSlackIntegration-KTAK1u"
 }
 
 data "aws_secretsmanager_secret_version" "np" {
-    secret_id = data.aws_secretsmanager_secret.np.id
+  secret_id = data.aws_secretsmanager_secret.np.id
 }
 
 data "aws_secretsmanager_secret" "pr" {
-    arn = "arn:aws:secretsmanager:eu-west-2:216552277552:secret:DynatraceProductionSlackIntegration-AR5VOj"
+  arn = "arn:aws:secretsmanager:eu-west-2:216552277552:secret:DynatraceProductionSlackIntegration-AR5VOj"
 }
 
 data "aws_secretsmanager_secret_version" "pr" {
-    secret_id = data.aws_secretsmanager_secret.pr.id
+  secret_id = data.aws_secretsmanager_secret.pr.id
 }
--- problem_notifications.tf
+++ problem_notifications.tf
@@ -1,8 +1,8 @@
 resource "dynatrace_slack_notification" "slack_notification" {
-    active  = true
-    name    = "di-slack-notification"
-    profile = "di-alerting-profile"
-    url     = local.is_production ? jsondecode(data.aws_secretsmanager_secret_version.pr.secret_string)["SLACK_URL"] : jsondecode(data.aws_secretsmanager_secret_version.np.secret_string)["SLACK_URL"]
-    channel = local.is_production ? jsondecode(data.aws_secretsmanager_secret_version.pr.secret_string)["SLACK_CHANNEL"] : jsondecode(data.aws_secretsmanager_secret_version.np.secret_string)["SLACK_CHANNEL"]
-    message = "{State} {ProblemSeverity} Problem {ProblemID}: {ImpactedEntity}"
+  active  = true
+  name    = "di-slack-notification"
+  profile = "di-alerting-profile"
+  url     = local.is_production ? jsondecode(data.aws_secretsmanager_secret_version.pr.secret_string)["SLACK_URL"] : jsondecode(data.aws_secretsmanager_secret_version.np.secret_string)["SLACK_URL"]
+  channel = local.is_production ? jsondecode(data.aws_secretsmanager_secret_version.pr.secret_string)["SLACK_CHANNEL"] : jsondecode(data.aws_secretsmanager_secret_version.np.secret_string)["SLACK_CHANNEL"]
+  message = "{State} {ProblemSeverity} Problem {ProblemID}: {ImpactedEntity}"
 }
