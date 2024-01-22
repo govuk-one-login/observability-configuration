3d7693546e25a56c52b6f559c95a7120735be401
Pavels <pavels.zaborovskis@digital.cabinet-office.gov.uk>
2024-01-22 15:57:11 +0000

    OBS-154 add post office to service detection

--- service_detection.tf
+++ service_detection.tf
@@ -26,7 +26,7 @@ resource "dynatrace_service_external_web_request" "YOTI" {
     public_domain_name {
       enable_id_contributor = true
       service_id_contributor {
-        contribution_type = "OriginalValue"
+        contribution_type   = "OriginalValue"
         copy_from_host_name = true
       }
     }
@@ -54,7 +54,7 @@ resource "dynatrace_service_external_web_request" "Ordnance_Survey" {
     public_domain_name {
       enable_id_contributor = true
       service_id_contributor {
-        contribution_type = "OriginalValue"
+        contribution_type   = "OriginalValue"
         copy_from_host_name = true
       }
     }
@@ -232,8 +232,33 @@ resource "dynatrace_service_external_web_request" "AWS" {
   }
 }
 
-
-
+resource "dynatrace_service_external_web_request" "Post_Office" {
+  name    = "Post Office"
+  enabled = true
+  conditions {
+    condition {
+      attribute              = "HostName"
+      compare_operation_type = "StringEndsWith"
+      text_values            = ["locations.pol-platform.co.uk"]
+    }
+  }
+  id_contributors {
+    port_for_service_id = false
+    application_id {
+      enable_id_contributor = false
+    }
+    context_root {
+      enable_id_contributor = false
+    }
+    public_domain_name {
+      enable_id_contributor = true
+      service_id_contributor {
+        contribution_type   = "OriginalValue"
+        copy_from_host_name = true
+      }
+    }
+  }
+}
 
 # Service detection rules for External Web Requests 
 resource "dynatrace_service_external_web_service" "Redis" {
