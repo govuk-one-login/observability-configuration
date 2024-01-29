36cad69b60a85dbcfca816d6552ac12a80d22fce
Pavels <pavels.zaborovskis@digital.cabinet-office.gov.uk>
2024-01-29 15:31:23 +0000

    add IP determination

--- settings_web_mobile_monitoring.tf
+++ settings_web_mobile_monitoring.tf
@@ -10,6 +10,43 @@ resource "dynatrace_user_experience_score" "user_experience_score" {
 
 # Usability analytics
 resource "dynatrace_usability_analytics" "usability_analytics" {
-    detect_rage_clicks = true
+  detect_rage_clicks = true
 }
 
+# Not in use - User session custom metrics 
+# Not in use - User action custom metrics 
+# Not in use - Map IP addresses to locations 
+
+# IP determination
+resource "dynatrace_rum_ip_determination" "rproxy_remote_address" {
+  header_name = "rproxy_remote_address"
+}
+
+resource "dynatrace_rum_ip_determination" "true_client_ip" {
+  header_name = "True-Client-IP"
+}
+
+resource "dynatrace_rum_ip_determination" "x_client_ip" {
+  header_name = "X-Client-Ip"
+}
+
+resource "dynatrace_rum_ip_determination" "x_cluster_client_ip" {
+  header_name = "X-Cluster-Client-Ip"
+}
+
+resource "dynatrace_rum_ip_determination" "x_forwarded_for" {
+  header_name = "X-Forwarded-For"
+}
+
+resource "dynatrace_rum_ip_determination" "forwarded" {
+  count       = local.is_production ? 1 : 0
+  header_name = "Forwarded"
+}
+
+resource "dynatrace_rum_ip_determination" "x_http_client_ip" {
+  header_name = "X-Http-Client-Ip"
+}
+
+resource "dynatrace_rum_ip_determination" "cf_connecting_ip" {
+  header_name = "CF-Connecting-IP"
+}
