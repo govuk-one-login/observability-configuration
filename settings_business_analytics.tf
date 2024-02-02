79ea4ec5213b6552178eb3e84b464495bb397e73
Nicola Cassidy <115652373+NicolaCassidy@users.noreply.github.com>
2024-02-02 11:05:45 +0000

    Delete settings_business_analytics.tf
    removing business analytics file

--- settings_business_analytics.tf
+++
@@ -1,6 +0,0 @@
-resource "dynatrace_business_events_buckets" "business_events_buckets" {
-  enabled     = true
-  bucket_name = "default_bizevents"
-  matcher     = "*"
-  rule_name   = "default"
-}
