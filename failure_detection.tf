de381ac976e825c698fc26b44e56cd6b021c862a
Joe Roberts <joe.roberts@digital.cabinet-office.gov.uk>
2024-03-05 11:58:37 +0000

    Add rule to ignore 'Page not found' errors

---
+++ failure_detection.tf
@@ -0,0 +1,44 @@
+resource "dynatrace_failure_detection_parameters" "ignore_page_not_found" {
+  name = "ignore-page-not-found"
+
+  broken_links {
+    http_404_not_found_failures = false
+  }
+
+  http_response_codes {
+    client_side_errors                        = "500-599"
+    server_side_errors                        = "500-599"
+    fail_on_missing_response_code_client_side = false
+    fail_on_missing_response_code_server_side = false
+  }
+
+  exception_rules {
+    ignore_all_exceptions         = false
+    ignore_span_failure_detection = false
+
+    ignored_exceptions {
+      custom_handled_exception {
+        message_pattern = "Page not found"
+      }
+    }
+  }
+}
+
+resource "dynatrace_failure_detection_rules" "ignore_page_not_found" {
+  name         = "ignore-page-not-found"
+  enabled      = true
+  parameter_id = dynatrace_failure_detection_parameters.ignore_page_not_found.id
+
+  conditions {
+    condition {
+      attribute = "SERVICE_TYPE"
+      predicate {
+        predicate_type = "SERVICE_TYPE_EQUALS"
+        service_type = [
+          "WebRequest",
+          "WebService"
+        ]
+      }
+    }
+  }
+}
