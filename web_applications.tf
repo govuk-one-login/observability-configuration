983ec6fb31cbe7baf3901049c26e35d4fc3c4801
Joe Roberts <joe.roberts@digital.cabinet-office.gov.uk>
2024-04-24 10:41:47 +0100

    Add applications for each subdomains (list inexhaustive, add to later!)

--- web_applications.tf
+++ web_applications.tf
@@ -1,3 +1,21 @@
+locals {
+  one_login_subdomains = [
+    "signin",
+    "home",
+    "identity",
+    "review-a",
+    "review-b",
+    "review-bav",
+    "review-c",
+    "review-d",
+    "review-f",
+    "review-hc",
+    "review-k",
+    "review-o",
+    "review-pa"
+  ]
+}
+
 module "web_application_staging" {
   count  = var.environment == "nonproduction" ? 1 : 0
   source = "./modules/web_application"
@@ -6,7 +24,10 @@ module "web_application_staging" {
   opt_in_enabled = false
 }
 
-moved {
-  from = module.web_application_signin_staging
-  to   = module.web_application_staging
+module "web_application_staging_subdomains" {
+  for_each = var.environment == "nonproduction" ? [] : toset(locals.one_login_subdomains)
+  source   = "./modules/web_application"
+
+  hostname       = "${each.key}.staging.account.gov.uk"
+  opt_in_enabled = false
 }
