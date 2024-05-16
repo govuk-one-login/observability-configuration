aba16bd86be487e4de6402d07a8f8e3c3a432e25
Andy Goldschmidt <146747225+angold5@users.noreply.github.com>
2024-05-16 10:08:23 +0100

    Update slos.tf - cloud.aws.apigateway
    Update slos.tf - cloud.aws.apigateway
    
    Signed-off-by: Andy Goldschmidt <146747225+angold5@users.noreply.github.com>

--- slos.tf
+++ slos.tf
@@ -5,7 +5,7 @@ resource "dynatrace_slo_v2" "perf001" {
   evaluation_type    = "AGGREGATE"
   evaluation_window  = "-1M"
   filter             = "type(CUSTOM_DEVICE)"
-  metric_expression  = "((ext:cloud.aws.apiGateway.latency:avg:partition(\"latency\",value(\"good\",le(1000))):splitBy():count:default(0))/(ext:cloud.aws.apiGateway.latency:avg:splitBy():count)*(100)):default(100,always)"
+  metric_expression  = "((cloud.aws.apigateway.latencyByAccountIdApiNameRegionStage:avg:partition("\~"latency\~"",value("\~"good\~"",le(1000))):splitBy():count:default(0))/(cloud.aws.apigateway.latencyByAccountIdApiNameRegionStage:avg:splitBy():count)*(100)):default(100,always)"
   metric_name        = "nfr_perf001"
   target_success     = 95
   target_warning     = 97
@@ -22,7 +22,7 @@ resource "dynatrace_slo_v2" "perf002" {
   evaluation_type    = "AGGREGATE"
   evaluation_window  = "-1M"
   filter             = "type(CUSTOM_DEVICE)"
-  metric_expression  = "((ext:cloud.aws.apiGateway.latency:avg:partition(\"latency\",value(\"good\",le(2500))):splitBy():count:default(0))/(ext:cloud.aws.apiGateway.latency:avg:splitBy():count)*(100)):default(100,always)"
+  metric_expression  = "((cloud.aws.apigateway.latencyByAccountIdApiNameRegionStage:avg:partition("\~"latency\~"",value("\~"good\~"",le(2500))):splitBy():count:default(0))/(cloud.aws.apigateway.latencyByAccountIdApiNameRegionStage:avg:splitBy():count)*(100)):default(100,always)"
   metric_name        = "nfr_perf002"
   target_success     = 99
   target_warning     = 99.5
