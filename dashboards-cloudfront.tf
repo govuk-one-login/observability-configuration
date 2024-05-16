a973d5a9db7ac6c3052b3d5dd8d89e4f78fd8ad6
Viveak <viveak.vadivelkarasan@digital.cabinet-office.gov.uk>
2024-05-16 11:58:35 +0100

    Kiwi CF Requests Dashboard

--- dashboards-cloudfront.tf
+++ dashboards-cloudfront.tf
@@ -2,26 +2,26 @@ variable "teamscloudfront1" {
   description = "map"
   default = {
     "team-a" = {
-      owneremail = "team-a@company.org"
-      teamname   = "Kiwi - di-bav-cri"
+      owneremail = "viveak.vadivelkarasan@digital.cabinet-office.gov.uk"
+      teamname   = "Kiwi - BAV-CRI"
       awsaccprod = "096369912800"
       awsaccint  = "603505715695"
     }
     "team-b" = {
-      owneremail = "team-a@company.org"
-      teamname   = "Kiwi - di-facetoface-cri"
+      owneremail = "viveak.vadivelkarasan@digital.cabinet-office.gov.uk"
+      teamname   = "Kiwi - F2F-CRI"
       awsaccprod = "377086294028"
       awsaccint  = "766319219145"
     }
     "team-c" = {
-      owneremail = "team-a@company.org"
-      teamname   = "Kiwi - di-facetoface"
+      owneremail = "viveak.vadivelkarasan@digital.cabinet-office.gov.uk"
+      teamname   = "Kiwi - CIC-CRI"
       awsaccprod = "270790667266"
       awsaccint  = "341874137503"
     }
     "team-d" = {
-      owneremail = "team-a@company.org"
-      teamname   = "Kiwi - di-ipvreturn"
+      owneremail = "viveak.vadivelkarasan@digital.cabinet-office.gov.uk"
+      teamname   = "Kiwi - IPVReturn-CRI"
       awsaccprod = "821078365651"
       awsaccint  = "988112579449"
     }
