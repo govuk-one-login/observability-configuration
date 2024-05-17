622c04139cd4bc0db51a4411581c248a068e14a1
Joe Roberts <joe.roberts@digital.cabinet-office.gov.uk>
2024-05-17 13:18:07 +0100

    Ignore 'invalid csrf token' errors
    Signed-off-by: Joe Roberts <joe.roberts@digital.cabinet-office.gov.uk>

--- failure_detection.tf
+++ failure_detection.tf
@@ -42,3 +42,48 @@ resource "dynatrace_failure_detection_rules" "ignore_page_not_found" {
     }
   }
 }
+
+resource "dynatrace_failure_detection_parameters" "ignore_invalid_csrf_token" {
+  name = "ignore-invalid-csrf-token"
+
+  broken_links {
+    http_404_not_found_failures = false
+  }
+
+  http_response_codes {
+    client_side_errors                        = "400-599"
+    server_side_errors                        = "500-599"
+    fail_on_missing_response_code_client_side = false
+    fail_on_missing_response_code_server_side = false
+  }
+
+  exception_rules {
+    ignore_all_exceptions         = false
+    ignore_span_failure_detection = false
+
+    ignored_exceptions {
+      custom_handled_exception {
+        message_pattern = "invalid csrf token"
+      }
+    }
+  }
+}
+
+resource "dynatrace_failure_detection_rules" "ignore_invalid_csrf_token" {
+  name         = "ignore-invalid-csrf-token"
+  enabled      = true
+  parameter_id = dynatrace_failure_detection_parameters.ignore_invalid_csrf_token.id
+
+  conditions {
+    condition {
+      attribute = "SERVICE_TYPE"
+      predicate {
+        predicate_type = "SERVICE_TYPE_EQUALS"
+        service_type = [
+          "WebRequest",
+          "WebService"
+        ]
+      }
+    }
+  }
+}
