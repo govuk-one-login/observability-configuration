096eb1dc68d35868bcd7c603f2efea75507bedf1
chrisdodd93 <christopher.dodd@digital.cabinet-office.gov.uk>
2024-07-17 08:49:18 +0100

    Updating SLO module and amending codeowners

--- dashboards.tf
+++ dashboards.tf
@@ -20,26 +20,10 @@ resource "dynatrace_dashboard_sharing" "signin-signup" {
   }
 }
 
-resource "dynatrace_json_dashboard" "slos" {
-  contents = file("${path.module}/dashboards/slos.json")
-}
-
-resource "dynatrace_dashboard_sharing" "slos" {
-  dashboard_id = dynatrace_json_dashboard.slos.id
-
-  enabled = true
-
-  permissions {
-    permission {
-      level = "VIEW"
-      type  = "ALL"
-    }
-    permission {
-      id    = data.dynatrace_iam_group.all.id
-      level = "VIEW"
-      type  = "GROUP"
-    }
-  }
+module "slo_dashboard" {
+  count  = local.is_production ? 1 : 0
+  source = "./modules/dashboard"
+  path   = "slos.json"
 }
 
 module "service_dashboard" {
