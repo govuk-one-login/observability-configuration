c5912a700166207e3e263c2dcb2cf88150f5a3d9
Karol Gancarz <karol.gancarz@digital.cabinet-office.gov.uk>
2024-09-12 11:15:19 +0100

    BAU: Add newlines for readability

--- dashboards.tf
+++ dashboards.tf
@@ -155,6 +155,7 @@ module "di_auth_check_experian_build_dashboard" {
   check_account_id        = "851725166715"
   application_environment = "build"
 }
+
 module "di_auth_check_experian_staging_dashboard" {
   count  = local.is_production ? 0 : 1
   source = "./dashboards/authentication/di-auth-check-experian"
@@ -163,6 +164,7 @@ module "di_auth_check_experian_staging_dashboard" {
   check_account_id        = "891377189576"
   application_environment = "staging"
 }
+
 module "di_auth_check_experian_integration_dashboard" {
   count  = local.is_production ? 0 : 1
   source = "./dashboards/authentication/di-auth-check-experian"
@@ -171,6 +173,7 @@ module "di_auth_check_experian_integration_dashboard" {
   check_account_id        = "211125427676"
   application_environment = "integration"
 }
+
 module "di_auth_check_experian_production_dashboard" {
   count  = local.is_production ? 1 : 0
   source = "./dashboards/authentication/di-auth-check-experian"
