416edc9686720d7d1c8189f62e3e4f316d644908
chrisdodd93 <christopher.dodd@digital.cabinet-office.gov.uk>
2024-10-11 13:43:47 +0100

    Added in new global suppression rule for invalid session

--- settings_server_side_service_monitoring.tf
+++ settings_server_side_service_monitoring.tf
@@ -738,6 +738,32 @@ resource "dynatrace_failure_detection_parameters" "ignore_missing_prereq_for_thi
   }
 }
 
+resource "dynatrace_failure_detection_parameters" "invalid_session" {
+  name = "invalid-session"
+
+  broken_links {
+    http_404_not_found_failures = false
+  }
+
+  http_response_codes {
+    client_side_errors                        = "400-599"
+    server_side_errors                        = "500-599"
+    fail_on_missing_response_code_client_side = false
+    fail_on_missing_response_code_server_side = false
+  }
+
+  exception_rules {
+    ignore_all_exceptions         = false
+    ignore_span_failure_detection = false
+
+    ignored_exceptions {
+      custom_handled_exception {
+        message_pattern = "Invalid session and referrer does not have gov.uk domain"
+      }
+    }
+  }
+}
+
 # Failure detection rules
 resource "dynatrace_failure_detection_rules" "ignore_page_not_found" {
   name         = "ignore-page-not-found"
@@ -781,6 +807,25 @@ resource "dynatrace_failure_detection_rules" "ignore_missing_prereq_for_this_ste
   name         = "ignore-missing-prereq-for-this-step"
   enabled      = true
   parameter_id = dynatrace_failure_detection_parameters.ignore_missing_prereq_for_this_step.id
+
+  conditions {
+    condition {
+      attribute = "SERVICE_TYPE"
+      predicate {
+        predicate_type = "SERVICE_TYPE_EQUALS"
+        service_type = [
+          "WebRequest",
+          "WebService"
+        ]
+      }
+    }
+  }
+}
+
+resource "dynatrace_failure_detection_rules" "invalid_session" {
+  name         = "invalid-session"
+  enabled      = true
+  parameter_id = dynatrace_failure_detection_parameters.invalid_session.id
 
   conditions {
     condition {
