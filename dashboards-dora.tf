5f80eca15ba5c1029a0a84df0a289b74719a059a
fpmrqs <francisco.pottingermarques@digital.cabinet-office.gov.uk>
2024-10-11 12:17:10 +0100

    PSREDEV-1826: added dashboars terraform resources

--- dashboards-dora.tf
+++ dashboards-dora.tf
@@ -7,6 +7,10 @@ locals {
   secure_pipelines_multiple_sam_stack_names = {
     for f in csvdecode(file("./dashboards/dev-platform/pipelines-list-multi-sam-stack-names.csv")) : "${f.team}" => f...
   }
+
+  secure_pipelines_list = {
+    for f in csvdecode(file("./dashboards/dev-platform/pipelines-list.csv")) : "${f.team}" => f...
+  }
 }
 
 ########################################
@@ -26,10 +30,43 @@ resource "dynatrace_json_dashboard" "Team-DORA-Dashboards4" {
   })
 }
 
+resource "dynatrace_json_dashboard" "Team-DORA-Dashboards" {
+  for_each = local.secure_pipelines_list
+
+  contents = templatefile("./dashboards/dev-platform/teams_pipeline_dora_dashboard_multiple_ssn.tftpl", {
+    team                       = each.key
+    owner                      = each.value[0].email
+    application_name           = [for app in each.value : app.application_name]
+    sam_stack_name_build       = [for app in each.value : app.sam_stack_name_build]
+    sam_stack_name_staging     = [for app in each.value : app.sam_stack_name_staging]
+    sam_stack_name_production  = [for app in each.value : app.sam_stack_name_production]
+    sam_stack_name_integration = [for app in each.value : app.sam_stack_name_integration]
+  })
+}
+
 resource "dynatrace_dashboard_sharing" "Team-DORA-Dashboards4" {
   for_each = local.secure_pipelines_multiple_sam_stack_names
 
   dashboard_id = dynatrace_json_dashboard.Team-DORA-Dashboards4[each.key].id
+  enabled      = true
+  permissions {
+    permission {
+      level = "VIEW"
+      type  = "ALL"
+    }
+    permission {
+      id    = data.dynatrace_iam_group.all.id
+      level = "VIEW"
+      type  = "GROUP"
+    }
+  }
+
+}
+
+resource "dynatrace_dashboard_sharing" "Team-DORA-Dashboards" {
+  for_each = local.secure_pipelines_list
+
+  dashboard_id = dynatrace_json_dashboard.Team-DORA-Dashboards[each.key].id
   enabled      = true
   permissions {
     permission {
