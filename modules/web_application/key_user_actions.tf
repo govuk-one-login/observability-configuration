b0349dd79ed7e5f8d1985cb5bf4377a7f35657d3
chrisdodd93 <christopher.dodd@digital.cabinet-office.gov.uk>
2024-11-29 11:51:13 +0000

    Addition of new key user actions

--- modules/web_application/key_user_actions.tf
+++ modules/web_application/key_user_actions.tf
@@ -71,10 +71,98 @@ resource "dynatrace_key_user_action" "enter_code" {
 }
 
 # Address CRI Key User Actions
+resource "dynatrace_key_user_action" "results" {
+  count          = strcontains(var.hostname, "review-a") ? 1 : 0
+  application_id = dynatrace_web_application.web_application.id
+  name           = "loading of page /results"
+  type           = "Load"
+}
+
+resource "dynatrace_key_user_action" "confirm" {
+  count          = strcontains(var.hostname, "review-a") ? 1 : 0
+  application_id = dynatrace_web_application.web_application.id
+  name           = "loading of page /summary/confirm"
+  type           = "Load"
+}
+
+resource "dynatrace_key_user_action" "search" {
+  count          = strcontains(var.hostname, "review-a") ? 1 : 0
+  application_id = dynatrace_web_application.web_application.id
+  name           = "loading of page /search"
+  type           = "Load"
+}
+
+resource "dynatrace_key_user_action" "address" {
+  count          = strcontains(var.hostname, "review-a") ? 1 : 0
+  application_id = dynatrace_web_application.web_application.id
+  name           = "loading of page /address"
+  type           = "Load"
+}
+
+resource "dynatrace_key_user_action" "click_address" {
+  count          = strcontains(var.hostname, "review-a") ? 1 : 0
+  application_id = dynatrace_web_application.web_application.id
+  name           = "click on continue in /address"
+  type           = "xhr"
+}
+
+resource "dynatrace_key_user_action" "click_confirm" {
+  count          = strcontains(var.hostname, "review-a") ? 1 : 0
+  application_id = dynatrace_web_application.web_application.id
+  name           = "click on i confirm my details are correct in /summary/confirm"
+  type           = "xhr"
+}
 
 # Fraud Key User Actions
+resource "dynatrace_key_user_action" "check" {
+  count          = strcontains(var.hostname, "review-f") ? 1 : 0
+  application_id = dynatrace_web_application.web_application.id
+  name           = "loading of page /check"
+  type           = "load"
+}
+
+resource "dynatrace_key_user_action" "click_check" {
+  count          = strcontains(var.hostname, "review-f") ? 1 : 0
+  application_id = dynatrace_web_application.web_application.id
+  name           = "click on continue in /check"
+  type           = "xhr"
+}
 
 # Home Key User Actions
+resource "dynatrace_key_user_action" "your_services" {
+  count          = strcontains(var.hostname, "home") ? 1 : 0
+  application_id = dynatrace_web_application.web_application.id
+  name           = "loading of page /your-services"
+  type           = "load"
+}
+
+resource "dynatrace_key_user_action" "contact_one_login" {
+  count          = strcontains(var.hostname, "home") ? 1 : 0
+  application_id = dynatrace_web_application.web_application.id
+  name           = "loading of page /contact-gov-uk-one-login"
+  type           = "load"
+}
+
+resource "dynatrace_key_user_action" "security" {
+  count          = strcontains(var.hostname, "home") ? 1 : 0
+  application_id = dynatrace_web_application.web_application.id
+  name           = "loading of page /security"
+  type           = "load"
+}
+
+resource "dynatrace_key_user_action" "enter_password" {
+  count          = strcontains(var.hostname, "home") ? 1 : 0
+  application_id = dynatrace_web_application.web_application.id
+  name           = "loading of page /enter-password"
+  type           = "load"
+}
+
+resource "dynatrace_key_user_action" "account_not_found" {
+  count          = strcontains(var.hostname, "home") ? 1 : 0
+  application_id = dynatrace_web_application.web_application.id
+  name           = "loading of page /account-not-found"
+  type           = "load"
+}
 
 # Document App CRI Key User Actions
 
