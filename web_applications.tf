1be46058df1e62626f7581481581f0aba223ead5
chrisdodd93 <christopher.dodd@digital.cabinet-office.gov.uk>
2025-01-27 09:54:14 +0000

    Removing injection rules config to remove from state file, will re-add

--- web_applications.tf
+++ web_applications.tf
@@ -3,67 +3,67 @@ locals {
     signin = {
       hostname         = "signin"
       name             = "Authentication"
-      manual_injection = false
+      # manual_injection = false
     },
     home = {
       hostname         = "home"
       name             = "Home"
-      manual_injection = true
+      # manual_injection = true
     },
     identity = {
       hostname         = "identity",
       name             = "IPV Core"
-      manual_injection = true
+      # manual_injection = true
     },
     review-a = {
       hostname         = "review-a"
       name             = "Address CRI"
-      manual_injection = false
+      # manual_injection = false
     },
     review-b = {
       hostname         = "review-b"
       name             = "Document App CRI"
-      manual_injection = false
+      # manual_injection = false
     },
     review-bav = {
       hostname         = "review-bav"
       name             = "Bank Account Verification CRI"
-      manual_injection = false
+      # manual_injection = false
     },
     review-c = {
       hostname         = "review-c"
       name             = "Claimed Identity Collector CRI"
-      manual_injection = false
+      # manual_injection = false
     },
     review-d = {
       hostname         = "review-d"
       name             = "Driving License CRI"
-      manual_injection = false
+      # manual_injection = false
     },
     review-f = {
       hostname         = "review-f"
       name             = "Fraud CRI"
-      manual_injection = false
+      # manual_injection = false
     },
     review-hc = {
       hostname         = "review-hc"
       name             = "HMRC NINO Check CRI"
-      manual_injection = false
+      # manual_injection = false
     },
     review-k = {
       hostname         = "review-k"
       name             = "Experian KBV CRI"
-      manual_injection = false
+      # manual_injection = false
     },
     review-o = {
       hostname         = "review-o"
       name             = "Face to Face CRI"
-      manual_injection = false
+      # manual_injection = false
     },
     review-pa = {
       hostname         = "review-pa"
       name             = "Passport CRI"
-      manual_injection = false
+      # manual_injection = false
     }
   }
 }
@@ -76,7 +76,7 @@ module "web_application_staging" {
   name             = "Staging"
   enabled          = true
   opt_in_enabled   = true
-  manual_injection = false
+  # manual_injection = false
 }
 
 module "web_application_staging_subdomains" {
@@ -87,7 +87,7 @@ module "web_application_staging_subdomains" {
   name             = "${each.value["name"]} Staging"
   enabled          = true
   opt_in_enabled   = true
-  manual_injection = each.value["manual_injection"]
+  # manual_injection = each.value["manual_injection"]
 }
 
 module "web_application_integration" {
@@ -98,7 +98,7 @@ module "web_application_integration" {
   name             = "Integration"
   enabled          = true
   opt_in_enabled   = true
-  manual_injection = false
+  # manual_injection = false
 }
 
 module "web_application_integration_subdomains" {
@@ -109,7 +109,7 @@ module "web_application_integration_subdomains" {
   name             = "${each.value["name"]} Integration"
   enabled          = true
   opt_in_enabled   = true
-  manual_injection = each.value["manual_injection"]
+  # manual_injection = each.value["manual_injection"]
 }
 
 module "web_application_production" {
@@ -120,7 +120,7 @@ module "web_application_production" {
   name             = "Production"
   enabled          = true
   opt_in_enabled   = true
-  manual_injection = false
+  # manual_injection = false
 }
 
 module "web_application_production_subdomains" {
@@ -131,5 +131,5 @@ module "web_application_production_subdomains" {
   name             = "${each.value["name"]} Production"
   enabled          = true
   opt_in_enabled   = true
-  manual_injection = each.value["manual_injection"]
+  # manual_injection = each.value["manual_injection"]
 }
