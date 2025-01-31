318d97cb3afff50088f04c9a62792f19cf5cde67
chrisdodd93 <christopher.dodd@digital.cabinet-office.gov.uk>
2025-01-31 11:35:02 +0000

    Re-adding in injection rules for IPV Core and Home

--- web_applications.tf
+++ web_applications.tf
@@ -1,56 +1,69 @@
 locals {
   one_login_subdomains = {
     signin = {
-      hostname = "signin"
-      name     = "Authentication"
+      hostname         = "signin"
+      name             = "Authentication"
+      manual_injection = false
     },
     home = {
-      hostname = "home"
-      name     = "Home"
+      hostname         = "home"
+      name             = "Home"
+      manual_injection = true
     },
     identity = {
-      hostname = "identity"
-      name     = "IPV Core"
+      hostname         = "identity",
+      name             = "IPV Core"
+      manual_injection = true
     },
     review-a = {
-      hostname = "review-a"
-      name     = "Address CRI"
+      hostname         = "review-a"
+      name             = "Address CRI"
+      manual_injection = false
     },
     review-b = {
-      hostname = "review-b"
-      name     = "Document App CRI"
+      hostname         = "review-b"
+      name             = "Document App CRI"
+      manual_injection = false
     },
     review-bav = {
-      hostname = "review-bav"
-      name     = "Bank Account Verification CRI"
+      hostname         = "review-bav"
+      name             = "Bank Account Verification CRI"
+      manual_injection = false
     },
     review-c = {
-      hostname = "review-c"
-      name     = "Claimed Identity Collector CRI"
+      hostname         = "review-c"
+      name             = "Claimed Identity Collector CRI"
+      manual_injection = false
     },
     review-d = {
-      hostname = "review-d"
-      name     = "Driving License CRI"
+      hostname         = "review-d"
+      name             = "Driving License CRI"
+      manual_injection = false
     },
     review-f = {
-      hostname = "review-f"
-      name     = "Fraud CRI"
+      hostname         = "review-f"
+      name             = "Fraud CRI"
+      manual_injection = false
     },
     review-hc = {
-      hostname = "review-hc"
-      name     = "HMRC NINO Check CRI"
+      hostname         = "review-hc"
+      name             = "HMRC NINO Check CRI"
+      manual_injection = false
     },
     review-k = {
-      hostname = "review-k"
-      name     = "Experian KBV CRI"
+      hostname         = "review-k"
+      name             = "Experian KBV CRI"
+      manual_injection = false
     },
     review-o = {
-      hostname = "review-o"
-      name     = "Face to Face CRI"
+      hostname         = "review-o"
+      name             = "Face to Face CRI"
+      manual_injection = false
     },
     review-pa = {
-      hostname = "review-pa"
-      name     = "Passport CRI"
+      hostname         = "review-pa"
+      name             = "Passport CRI"
+      manual_injection = false
     }
   }
 }
@@ -59,58 +72,64 @@ module "web_application_staging" {
   count  = var.environment == "nonproduction" ? 1 : 0
   source = "./modules/web_application"
 
-  hostname       = "staging.account.gov.uk"
-  name           = "Staging"
-  enabled        = true
-  opt_in_enabled = true
+  hostname         = "staging.account.gov.uk"
+  name             = "Staging"
+  enabled          = true
+  opt_in_enabled   = true
+  manual_injection = false
 }
 
 module "web_application_staging_subdomains" {
   for_each = var.environment == "nonproduction" ? local.one_login_subdomains : {}
   source   = "./modules/web_application"
 
-  hostname       = "${each.value["hostname"]}.staging.account.gov.uk"
-  name           = "${each.value["name"]} Staging"
-  enabled        = true
-  opt_in_enabled = true
+  hostname         = "${each.value["hostname"]}.staging.account.gov.uk"
+  name             = "${each.value["name"]} Staging"
+  enabled          = true
+  opt_in_enabled   = true
+  manual_injection = each.value["manual_injection"]
 }
 
 module "web_application_integration" {
   count  = var.environment == "nonproduction" ? 1 : 0
   source = "./modules/web_application"
 
-  hostname       = "integration.account.gov.uk"
-  name           = "Integration"
-  enabled        = true
-  opt_in_enabled = true
+  hostname         = "integration.account.gov.uk"
+  name             = "Integration"
+  enabled          = true
+  opt_in_enabled   = true
+  manual_injection = false
 }
 
 module "web_application_integration_subdomains" {
   for_each = var.environment == "nonproduction" ? local.one_login_subdomains : {}
   source   = "./modules/web_application"
 
-  hostname       = "${each.value["hostname"]}.integration.account.gov.uk"
-  name           = "${each.value["name"]} Integration"
-  enabled        = true
-  opt_in_enabled = true
+  hostname         = "${each.value["hostname"]}.integration.account.gov.uk"
+  name             = "${each.value["name"]} Integration"
+  enabled          = true
+  opt_in_enabled   = true
+  manual_injection = each.value["manual_injection"]
 }
 
 module "web_application_production" {
   count  = var.environment == "production" ? 1 : 0
   source = "./modules/web_application"
 
-  hostname       = "account.gov.uk"
-  name           = "Production"
-  enabled        = true
-  opt_in_enabled = true
+  hostname         = "account.gov.uk"
+  name             = "Production"
+  enabled          = true
+  opt_in_enabled   = true
+  manual_injection = false
 }
 
 module "web_application_production_subdomains" {
   for_each = var.environment == "production" ? local.one_login_subdomains : {}
   source   = "./modules/web_application"
 
-  hostname       = "${each.value["hostname"]}.account.gov.uk"
-  name           = "${each.value["name"]} Production"
-  enabled        = true
-  opt_in_enabled = true
+  hostname         = "${each.value["hostname"]}.account.gov.uk"
+  name             = "${each.value["name"]} Production"
+  enabled          = true
+  opt_in_enabled   = true
+  manual_injection = each.value["manual_injection"]
 }
