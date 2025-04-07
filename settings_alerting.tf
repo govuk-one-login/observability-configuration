0c44d420209d4bce19bcedb255c5bb0a889a42ca
Ryan Hynes <ryan.hynes@digital.cabinet-office.gov.uk>
2025-04-07 11:18:53 +0100

    Added alerting profile for TEAM.

--- settings_alerting.tf
+++ settings_alerting.tf
@@ -49,3 +49,54 @@ resource "dynatrace_alerting" "di-alerting-profile" {
   }
 
 }
+
+resource "dynatrace_alerting" "di-team-alerting" {
+  name            = "T.E.A.M. Alerting"
+  management_zone = ""
+  rules {
+    rule {
+      include_mode     = "NONE"
+      delay_in_minutes = 0
+      severity_level   = "AVAILABILITY"
+    }
+    rule {
+      include_mode     = "NONE"
+      delay_in_minutes = 0
+      severity_level   = "CUSTOM_ALERT"
+    }
+    rule {
+      include_mode     = "NONE"
+      delay_in_minutes = 0
+      severity_level   = "ERRORS"
+    }
+    rule {
+      include_mode     = "NONE"
+      delay_in_minutes = 0
+      severity_level   = "MONITORING_UNAVAILABLE"
+    }
+    rule {
+      include_mode     = "NONE"
+      delay_in_minutes = 0
+      severity_level   = "PERFORMANCE"
+    }
+    rule {
+      include_mode     = "NONE"
+      delay_in_minutes = 30
+      severity_level   = "RESOURCE_CONTENTION"
+    }
+  }
+
+  filters {
+    filter {
+      custom {
+        title {
+          operator = "CONTAINS"
+          value = "TEAM"
+          case_sensitive = true
+          enabled = true
+        }
+      }
+    }
+  }
+}
+
