1c8c2e7fc95a49671a9956fa5a33d8d9a6dd25ce
Ryan Hynes <ryan.hynes@digital.cabinet-office.gov.uk>
2025-04-07 11:38:09 +0100

    Added a slack notification for the TEAM alerting profile.

--- settings_integration.tf
+++ settings_integration.tf
@@ -7,3 +7,12 @@ resource "dynatrace_slack_notification" "slack_notification" {
   channel = local.is_production ? jsondecode(data.aws_secretsmanager_secret_version.pr.secret_string)["SLACK_CHANNEL"] : jsondecode(data.aws_secretsmanager_secret_version.np.secret_string)["SLACK_CHANNEL"]
   message = "{State} {ProblemSeverity} Problem {ProblemID}: {ImpactedEntity}"
 }
+
+resource "dynatrace_slack_notification" "team_slack_notification" {
+  active  = true
+  name    = "TEAM Slack Alerts"
+  profile = dynatrace_alerting.di-team-alerting.id
+  url     = local.is_production ? jsondecode(data.aws_secretsmanager_secret_version.pr.secret_string)["TEAM_SLACK_URL"] : jsondecode(data.aws_secretsmanager_secret_version.np.secret_string)["TEAM_SLACK_URL"]
+  channel = local.is_production ? jsondecode(data.aws_secretsmanager_secret_version.pr.secret_string)["TEAM_SLACK_CHANNEL"] : jsondecode(data.aws_secretsmanager_secret_version.np.secret_string)["TEAM_SLACK_CHANNEL"]
+  message = "{State} {ProblemSeverity} Problem {ProblemID}: {ImpactedEntity}"
+}
