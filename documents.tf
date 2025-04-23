0276a992a38f476d5cd8ce76c4612d8206298628
chrisdodd93 <christopher.dodd@digital.cabinet-office.gov.uk>
2025-04-23 12:07:20 +0100

    Fixing formatting error

--- documents.tf
+++ documents.tf
@@ -1,12 +1,12 @@
 module "fpad_infrastructre_dashboard" {
-  source = "./modules/documents"
+  source        = "./modules/documents"
   document_path = "fpad/fpad_infrastructure_dashboard.json"
   document_name = "FPAD Infrastructre Dashboard"
   document_type = "dashboard"
 }
 
 module "apigw_metrics" {
-  source = "./modules/documents"
+  source        = "./modules/documents"
   document_path = "core/apigw_metrics.json"
   document_name = "IPV Core APIGW Metrics"
   document_type = "dashboard"
