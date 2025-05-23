14c8f82b78b78f42d871c810428519b22c00995c
Matthew Barber <matthew.barber@digital.cabinet-office.gov.uk>
2025-05-23 13:32:34 +0100

    DCMAW-12597 fix dashboard naming

--- dashboards.tf
+++ dashboards.tf
@@ -671,10 +671,10 @@ module "async_backend_completion_rate" {
   path   = "id-check/async-backend-completion-rate.json"
 }
 
-module "async_api_metrics" {
+module "async_backend_api_gateway" {
   count  = local.is_production ? 1 : 0 # Only create in production
   source = "./modules/dashboard"
-  path   = "id-check/async-api-metrics.json"
+  path   = "id-check/async-backend-api-gateway.json"
 }
 
 ### MOBILE PLATFORM ###
