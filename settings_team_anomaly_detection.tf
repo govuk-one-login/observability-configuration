07fecace17eb9fb5a1956931110427b139c4cc2d
Rory Sedgwick <rory.sedgwick@digital.cabinet-office.gov.uk>
2025-06-06 14:00:00 +0100

    Disable noisy TEAM appsync client disconnect error

--- settings_team_anomaly_detection.tf
+++ settings_team_anomaly_detection.tf
@@ -134,7 +134,7 @@ resource "dynatrace_metric_events" "team_appsync_connect_server_error" {
 
 resource "dynatrace_metric_events" "team_appsync_disconnect_client_error" {
   count   = local.is_production ? 1 : 0
-  enabled = true
+  enabled = false
   summary = "TEAM Appsync Disconnect Client Error Alert"
   event_template {
     description = <<-EOT
