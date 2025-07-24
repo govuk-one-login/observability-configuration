804a1f23e489ffad58d7d7207f86157822d1b4f9
Andy Brick <andy.brick@digital.cabinet-office.gov.uk>
2025-07-24 14:18:13 +0100

    Split dashboards into two separate files

--- dashboards.tf
+++ dashboards.tf
@@ -136,6 +136,18 @@ resource "dynatrace_dashboard_sharing" "aws_service_quotas" {
   }
 }
 
+### Fraud Pod ###
+
+module "fpad-ssf-infrastructure" {
+  source = "./modules/dashboard"
+  path   = "fraud/ssf/fpad-ssf-infrastructure.json"
+}
+
+module "fpad-ticf-cri-infrastructure" {
+  source = "./modules/dashboard"
+  path   = "fraud/ssf/fpad-ticf-cri-infrastructure.json"
+}
+
 ### Authentication ###
 
 module "di_auth_check_experian_build_dashboard" {
