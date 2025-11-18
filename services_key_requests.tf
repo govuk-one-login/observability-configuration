7632686dd15c7d664cc8b4b9ff8b1df9070fe4b0
Mike Beeby <mike.beeby@digital.cabinet-office.gov.uk>
2025-11-18 09:36:38 +0000

    feat: Add key requests for Orange CRI front services

--- services_key_requests.tf
+++ services_key_requests.tf
@@ -1,5 +1,65 @@
-# module "blah" {
-#   source               = "./modules/service_key_requests"
-#   key_requests_names   = ""
-#   key_requests_service = ""
-# }
+data "dynatrace_entity" "di-ipv-cri-check-hmrc-front" {
+  name = "di-ipv-cri-check-hmrc-front"
+  type = "SERVICE"
+}
+
+module "cri_check_hmrc_front_key_requests" {
+  source               = "./modules/service_key_requests"
+  key_requests_service = data.dynatrace_entity.di-ipv-cri-check-hmrc-front.id
+  key_requests_names = [
+    "/",
+    "/abandon",
+    "/could-not-match-national-insurance",
+    "/enter-national-insurance-number",
+    "/how-continue-national-insurance",
+    "/oauth2/authorize",
+    "/oauth2/callback"
+  ]
+}
+
+data "dynatrace_entity" "di-ipv-cri-address-front" {
+  name = "di-ipv-cri-address-front"
+  type = "SERVICE"
+}
+
+module "cri_address_front_key_requests" {
+  source               = "./modules/service_key_requests"
+  key_requests_service = data.dynatrace_entity.di-ipv-cri-address-front.id
+  key_requests_names = [
+    "/",
+    "/address",
+    "/address/edit",
+    "/enter-non-UK-address",
+    "/oauth2/authorize",
+    "/oauth2/callback",
+    "/prepopulate",
+    "/previous/address",
+    "/previous/problem",
+    "/previous/results",
+    "/previous/search",
+    "/problem",
+    "/results",
+    "/summary/confirm",
+    "/summary/non-UK-confirm",
+    "/what-country"
+  ]
+}
+
+data "dynatrace_entity" "di-ipv-cri-kbv-front" {
+  name = "di-ipv-cri-kbv-front"
+  type = "SERVICE"
+}
+
+module "cri_experian_kbv_front_key_requests" {
+  source               = "./modules/service_key_requests"
+  key_requests_service = data.dynatrace_entity.di-ipv-cri-kbv-front.id
+  key_requests_names = [
+    "/",
+    "/kbv/abandon",
+    "/kbv/done",
+    "/kbv/load-question",
+    "/kbv/question",
+    "/oauth2/authorize",
+    "/oauth2/callback"
+  ]
+}
