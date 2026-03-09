a8953db281d1ff58ab17b86073890bb9b5682ba6
Matt W <173997080+sw-mattw@users.noreply.github.com>
2026-03-09 13:37:43 +0000

    AUT-5281: Add auth international phone number dashboard

---
+++ dashboards/authentication/di-auth-international-phone-numbers/variables.tf
@@ -0,0 +1,12 @@
+variable "application_environment" {
+  description = "Environment of the application"
+}
+
+variable "account_id" {
+  description = "AWS Account ID"
+}
+
+
+variable "old_account_id" {
+  description = "Old AWS Account ID"
+}
